b90e67170c24cfe7df8a6fd9ecf68b4a1448b7e5
Julio Soto <juliosotodev@gmail.com>
2022-10-29 23:59:05 -0600

    variable types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int file_size = 0;
+    double sales = 9.99;
+    bool isActive = true;
+    char char01 = 'p';
+    float number = 4.56;
+    std::cout << char01 << std::endl;
     return 0;
 }
